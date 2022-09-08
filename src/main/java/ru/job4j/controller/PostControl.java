50c1e8cbdd4238bdd2de25149640bb90e1b343b6
Sergei Baranov <mariosb84@mail.ru>
2022-09-08 17:44:50 +0300

    7. HTML form. Создание вакансии. [#504844]

--- src/main/java/ru/job4j/controller/PostControl.java
+++ src/main/java/ru/job4j/controller/PostControl.java
@@ -4,6 +4,7 @@ package ru.job4j.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.model.Post;
 import ru.job4j.store.PostStore;
 
 @Controller
@@ -15,5 +16,10 @@ class PostController {
     public String posts(Model model) {
         model.addAttribute("posts", postStore.findAll());
         return "posts";
+    }
+    @GetMapping("/formAddPost")
+    public String addPost(Model model) {
+        model.addAttribute("post", new Post(0, "Заполните поле"));
+        return "addPost";
     }
 }
