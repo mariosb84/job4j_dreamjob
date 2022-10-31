956035b6e13393cf28ac24ce5f2ad2a1f33721f4
Sergei Baranov <mariosb84@mail.ru>
2022-10-31 12:40:45 +0300

    3. Тестирование базы данных. Liquibase H2 [#504862 #355781]

--- src/test/java/ru/job4j/CandidateDbStoreTest.java
+++ src/test/java/ru/job4j/CandidateDbStoreTest.java
@@ -5,6 +5,7 @@ import ru.job4j.model.Candidate;
 import ru.job4j.store.CandidateDbStore;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -13,9 +14,53 @@ public class CandidateDbStoreTest {
     @Test
     public void whenCreateCandidate() {
         CandidateDbStore store = new CandidateDbStore(new Main().loadPool());
-        Candidate candidate = new Candidate(0, "Ivan", "Ivan desc", LocalDateTime.now(), new byte[]{1, 2});
+        Candidate candidate = new Candidate(1, "Ivan0", "Ivan0 desc", LocalDateTime.now(), new byte[]{1, 2});
         store.add(candidate);
         Candidate candidateInDb = store.findById(candidate.getId());
         assertThat(candidateInDb.getName(), is(candidate.getName()));
+    }
+
+    @Test
+    public void whenFindAllCandidates() {
+        CandidateDbStore store = new CandidateDbStore(new Main().loadPool());
+        Candidate candidate = new Candidate(1, "Ivan1", "Ivan1 desc", LocalDateTime.now(), new byte[]{1, 2});
+        Candidate candidate2 = new Candidate(2, "Ivan2", "Ivan2 desc", LocalDateTime.now(), new byte[]{1, 3});
+        Candidate candidate3 = new Candidate(3, "Ivan3", "Ivan3 desc", LocalDateTime.now(), new byte[]{1, 4});
+        store.add(candidate);
+        store.add(candidate2);
+        store.add(candidate3);
+        List<Candidate> candidatesInDb = store.findAll();
+        assertThat(candidatesInDb.contains(candidate3), is(true));
+    }
+
+    @Test
+    public void whenUpdateCandidate() {
+        CandidateDbStore store = new CandidateDbStore(new Main().loadPool());
+        Candidate candidate = new Candidate(1, "Ivan4", "Ivan4 desc", LocalDateTime.now(), new byte[]{1, 2});
+        Candidate candidate2 = new Candidate(1, "Ivan5", "Ivan5 desc", LocalDateTime.now(), new byte[]{1, 3});
+        store.add(candidate);
+        store.update(candidate2);
+        Candidate candidateInDb = store.findById(candidate2.getId());
+        assertThat(candidateInDb.getName(), is(candidate2.getName()));
+    }
+
+    @Test
+    public void whenFindByIdCandidate() {
+        CandidateDbStore store = new CandidateDbStore(new Main().loadPool());
+        Candidate candidate = new Candidate(1, "Ivan6", "Ivan6 desc", LocalDateTime.now(), new byte[]{1, 2});
+        Candidate candidate2 = new Candidate(2, "Ivan7", "Ivan7 desc", LocalDateTime.now(), new byte[]{1, 3});
+        store.add(candidate);
+        store.add(candidate2);
+        Candidate candidateInDb = store.findById(candidate.getId());
+        assertThat(candidateInDb.getName(), is(candidate.getName()));
+    }
+
+    @Test
+    public void whenDeleteCandidate() {
+        CandidateDbStore store = new CandidateDbStore(new Main().loadPool());
+        Candidate candidate = new Candidate(1, "Ivan8", "Ivan8 desc", LocalDateTime.now(), new byte[]{1, 2});
+        store.add(candidate);
+        store.delete(candidate);
+        assertThat(store.findAll().contains(candidate), is(false));
     }
 }
--- src/test/java/ru/job4j/PostDBStoreTest.java
+++ src/test/java/ru/job4j/PostDBStoreTest.java
@@ -6,6 +6,7 @@ import ru.job4j.model.Post;
 import ru.job4j.store.PostDbStore;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -14,10 +15,54 @@ public class PostDBStoreTest {
     @Test
     public void whenCreatePost() {
         PostDbStore store = new PostDbStore(new Main().loadPool());
-        Post post = new Post(0, "Java Job", "Java Job Description",
+        Post post = new Post(1, "Java Job", "Java Job Description",
                 LocalDateTime.now(), true, new City(1, "Moscow"));
         store.add(post);
         Post postInDb = store.findById(post.getId());
         assertThat(postInDb.getName(), is(post.getName()));
+    }
+
+    @Test
+    public void whenFindAllPosts() {
+        PostDbStore store = new PostDbStore(new Main().loadPool());
+        Post post = new Post(1, "Post1", "Post1 desc", LocalDateTime.now(), true, new City(1, ""));
+        Post post2 = new Post(2, "Post2", "Post2 desc", LocalDateTime.now(), true, new City(2, ""));
+        Post post3 = new Post(3, "Post3", "Post3 desc", LocalDateTime.now(), true, new City(3, ""));
+        store.add(post);
+        store.add(post2);
+        store.add(post3);
+        List<Post> postInDb = store.findAll();
+        assertThat(postInDb.contains(post3), is(true));
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        PostDbStore store = new PostDbStore(new Main().loadPool());
+        Post post = new Post(1, "Post4", "Post4 desc", LocalDateTime.now(), true, new City(1, ""));
+        Post post2 = new Post(1, "Post5", "Post5 desc", LocalDateTime.now(), true, new City(2, ""));
+        store.add(post);
+        store.update(post2);
+        Post postInDb = store.findById(post2.getId());
+        assertThat(postInDb.getName(), is(post2.getName()));
+    }
+
+    @Test
+    public void whenFindByIdPost() {
+        PostDbStore store = new PostDbStore(new Main().loadPool());
+        Post post = new Post(1, "Post6", "Post6 desc", LocalDateTime.now(), true, new City(1, ""));
+        Post post2 = new Post(2, "Post7", "Post7 desc", LocalDateTime.now(), true, new City(2, ""));
+        store.add(post);
+        store.add(post2);
+        Post postInDb = store.findById(post2.getId());
+        assertThat(postInDb.getName(), is(post2.getName()));
+    }
+
+    @Test
+    public void whenDeletePost() {
+        PostDbStore store = new PostDbStore(new Main().loadPool());
+        Post post = new Post(1, "Post8", "Post8 desc", LocalDateTime.now(), true, new City(1, ""));
+        store.add(post);
+        store.delete(post);
+        assertThat(store.findAll().contains(post), is(false));
     }
 }
