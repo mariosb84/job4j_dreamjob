b223863c1a440ec020f468455406aa6c7f2c6541
Sergei Baranov <mariosb84@mail.ru>
2022-11-20 09:53:12 +0300

    2. Тестируем PostController [#504867 #362016]

--- src/test/java/ru/job4j/PostControllerTest.java
+++ src/test/java/ru/job4j/PostControllerTest.java
@@ -9,12 +9,9 @@ import ru.job4j.model.Post;
 import ru.job4j.service.CityService;
 import ru.job4j.service.PostService;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Enumeration;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -23,92 +20,7 @@ import static org.mockito.Mockito.*;
 
 public class PostControllerTest {
 
-    final static HttpSession SESSION = new HttpSession() {
-        @Override
-        public long getCreationTime() {
-            return 0;
-        }
-
-        @Override
-        public String getId() {
-            return null;
-        }
-
-        @Override
-        public long getLastAccessedTime() {
-            return 0;
-        }
-
-        @Override
-        public ServletContext getServletContext() {
-            return null;
-        }
-
-        @Override
-        public void setMaxInactiveInterval(int i) {
-
-        }
-
-        @Override
-        public int getMaxInactiveInterval() {
-            return 0;
-        }
-
-        @Override
-        public HttpSessionContext getSessionContext() {
-            return null;
-        }
-
-        @Override
-        public Object getAttribute(String s) {
-            return null;
-        }
-
-        @Override
-        public Object getValue(String s) {
-            return null;
-        }
-
-        @Override
-        public Enumeration<String> getAttributeNames() {
-            return null;
-        }
-
-        @Override
-        public String[] getValueNames() {
-            return new String[0];
-        }
-
-        @Override
-        public void setAttribute(String s, Object o) {
-
-        }
-
-        @Override
-        public void putValue(String s, Object o) {
-
-        }
-
-        @Override
-        public void removeAttribute(String s) {
-
-        }
-
-        @Override
-        public void removeValue(String s) {
-
-        }
-
-        @Override
-        public void invalidate() {
-
-        }
-
-        @Override
-        public boolean isNew() {
-            return false;
-        }
-    };
+    final static HttpSession SESSION = mock(HttpSession.class);
 
     @Test
     public void whenPosts() {
